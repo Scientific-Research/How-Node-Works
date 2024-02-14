16c6bc779f240564651b49feffe0eeea33899d88
Maximilian <scientificresearch309@gmail.com>
2024-02-14 23:50:56 +0100

    we don't have any class and the inastance too

--- modules.js
+++ modules.js
@@ -9,4 +9,8 @@ const calc1 = new C();
 console.log(calc1.add(2, 3));
 
 // exports
+const calc2 = require("./test-module-2"); // with this kind of exports, we can access to the
+// function directly and we don't have a class anymore => we don't need to create a new instance
+// of that class!
 
+console.log(calc2.divide(2, 3));
