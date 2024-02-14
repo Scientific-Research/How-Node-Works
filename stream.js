068f6ce4d067ae3fb2bb46d41097cd523bff03ee
Maximilian <scientificresearch309@gmail.com>
2024-02-14 22:51:49 +0100

    using the third Solution: piping the readableStreamData as input to the res as output directly without loss the data transfer speed!

--- stream.js
+++ stream.js
@@ -12,28 +12,32 @@ server.on("request", (req, res) => {
   //     }
   //     res.end(data);
   //   });
-
   // Solution 2: Stream
-  const readable = fs.createReadStream("test-file.txt"); // we read the data and as soon as it
-  // 's ready, we send it to the client=>res.write(chunk). readable.on() is observer or listener
-  // which listens to the createReadStream() and when a piece of the data is ready, it will
-  // get it and send it to the output!
-  // but in Solution 1, we have to wait till all the data is ready, once it is ready, it will
-  // sen dit to the output => res.end(data)
-  readable.on("data", (chunk) => {
-    res.write(chunk);
-  });
-
-  // we have to handle the event too, when all the data is there and streaming is finished!
-  readable.on("end", () => {
-    res.end(); // no more data will be write to this writable string!
-    // we don't send any data in res.end() unlike Solution 1.
-  });
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found");
-  });
+  //   const readable = fs.createReadStream("test-file.txt"); // we read the data and as soon as it
+  //   // 's ready, we send it to the client=>res.write(chunk). readable.on() is observer or listener
+  //   // which listens to the createReadStream() and when a piece of the data is ready, it will
+  //   // get it and send it to the output!
+  //   // but in Solution 1, we have to wait till all the data is ready, once it is ready, it will
+  //   // sen dit to the output => res.end(data)
+  //   readable.on("data", (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   // we have to handle the event too, when all the data is there and streaming is finished!
+  //   readable.on("end", () => {
+  //     res.end(); // no more data will be write to this writable string!
+  //     // we don't send any data in res.end() unlike Solution 1.
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+  // Solution 3 // the writing the data on Browser is not as fast as reading it from the source
+  // we have to use here pipe operator: => pipe make the data transfer very direct and the speed
+  // of both side stay in a very high speed!
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res);
+  // readableStreamSource.pipe(writableDestination)
 });
 
 // before to test it, we have to run the server first:
