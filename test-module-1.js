277b5eba4753aa57094a3933160f7f447a3bf3e6
Maximilian <scientificresearch309@gmail.com>
2024-02-14 23:42:52 +0100

    the second method to export the data to other file!

--- test-module-1.js
+++ test-module-1.js
@@ -1,4 +1,19 @@
-class Calculator {
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+
+// module.exports = Calculator; // we export our class and in modules.js will import it!
+// or in an elegant way, we can add exports to any of the methods in Calculator class!
+
+module.exports = class {
   add(a, b) {
     return a + b;
   }
@@ -8,6 +23,7 @@ class Calculator {
   divide(a, b) {
     return a / b;
   }
-}
-
-module.exports = Calculator; // we export our class and in modules.js will import it!
+};
+// we export our class and in modules.js will import it!
+// or in an elegant way, we can omit the name of the class and put the module.exports =
+// on behind of the class word!
