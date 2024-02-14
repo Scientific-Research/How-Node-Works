240feaf6028f0b643ca25486cf98bd12facc1ef1
Maximilian <scientificresearch309@gmail.com>
2024-02-14 23:46:08 +0100

    we created a new file with three exports separately for three functions

--- modules.js
+++ modules.js
@@ -1,8 +1,12 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+// module.exports
 const C = require("./test-module-1"); // we import our Calculator class here with a different name
 
 // defining a new calculaor instance from our Calculator class:
 const calc1 = new C();
 console.log(calc1.add(2, 3));
+
+// exports
+
